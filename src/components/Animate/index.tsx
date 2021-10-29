a652022f60446c5962bfa304bb599ce820fc5b47
王港 <wangg11@mingyuanyu.com>
2021-10-29 18:10:17 +0800

    准备增加动画组件

---
+++ src/components/Animate/index.tsx
@@ -0,0 +1,19 @@
+import classnames from "classnames";
+import React from "react";
+import style from "./index.module.less";
+import { AnimateElementProps, ElementCommonProps } from "./type";
+
+// export const createAnimateElement:React.FC<AnimateElementProps & ElementCommonProps> = (props=>{
+//     return <div></div>
+// })
+
+// export const AnimateElement: React.ComponentType<ElementCommonProps & AnimateElementProps> = (props:ElementCommonProps & AnimateElementProps) => {
+//   const { animateName, duration, className, ...restProps } = props;
+//   return (
+//     <div
+//       {...(restProps)}
+//       className={classnames(animateName && `animate__animated animate__${animateName}`, className)}
+//       style={{ ...(props.style as React.CSSProperties), "--animate-duration": `${duration}ms` }}
+//     />
+//   );
+// };
